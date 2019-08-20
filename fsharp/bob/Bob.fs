8d546f76821e82cf5f10df970c52cc362594b5ee
fomov <fomov.innovitive@gmail.com>
2019-08-20 15:43:05 +0300

    [Tasks]

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,20 @@
+﻿module Bob
+open System
+
+let response (input: string): string = 
+    let checkSign (input: string) = input.EndsWith("!")
+    let checkYell (input: string, num ) = 
+        input.ToCharArray() |> Array.toList |> List.filter(Char.IsUpper) |> Seq.length > num 
+    let checkQuestion (input: string) = input.EndsWith("?")
+
+    match input.Trim() with
+    | input when not (checkYell (input, 6)) && checkQuestion input -> "Sure."
+    | input when not(checkQuestion input) && (checkYell (input, 1) && checkSign input || checkYell (input, 6)) -> "Whoa, chill out!"
+    | input when checkYell (input, 6) && checkQuestion input -> "Calm down, I know what I'm doing!"
+    | input when input = "" -> "Fine. Be that way!"
+    | _ -> "Whatever."
+
+    
+    
+
+
