66b62b63c4384185a4f39e5f06c75b5af04f4f10
fomov <fomov.innovitive@gmail.com>
2019-08-29 13:14:45 +0300

    Bob - done

--- fsharp/bob/Bob.fs
+++ fsharp/bob/Bob.fs
@@ -1,29 +1,23 @@
 ﻿module Bob
 open System
 
-let checkSign (input: string) = input.EndsWith("!")
-
-let checkYell (input: string, num ) = 
-        input.ToCharArray() |> Array.toList |> List.filter(Char.IsUpper) |> Seq.length > num 
+let checkYell (input: string) = 
+         (input.ToCharArray() |> Array.toList |> List.filter(Char.IsLetter) |> Seq.length > 1) && input.ToUpper() = input
 
 let checkQuestion (input: string) = input.EndsWith("?")
 
-let (|Questio|Sighn|Yell|Silence|Any|) (input: string) = 
-    if checkQuestion input && not (checkYell (input, 6)) then Questio 
-    elif not(checkQuestion input) && (checkYell (input, 1) && checkSign input || checkYell (input, 6)) then Yell
-    elif checkYell (input, 6) && checkQuestion input then Sighn
+let (|Question|Exclamation|Yell|Silence|Another|) (input: string) = 
+    if checkYell input && checkQuestion input then Exclamation
+    elif checkQuestion input then Question 
+    elif checkYell input then Yell
     elif input = "" then Silence
-    else Any
+    else Another
 
 let response (input: string): string = 
     match input.Trim() with
-    | Questio -> "Sure."
+    | Question -> "Sure."
     | Yell  -> "Whoa, chill out!"
-    | Sighn  -> "Calm down, I know what I'm doing!"
+    | Exclamation  -> "Calm down, I know what I'm doing!"
     | Silence -> "Fine. Be that way!"
-    | _ -> "Whatever."
-
-    
-    
-
+    | Another -> "Whatever."
 
