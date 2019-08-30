0451449b090df95df5f5c24b05061a962085356b
fomov <fomov.innovitive@gmail.com>
2019-08-30 11:55:38 +0300

    reverce string done

---
+++ fsharp/reverse-string/ReverseString.fs
@@ -0,0 +1,10 @@
+﻿module ReverseString
+
+let reverse (input: string): string = 
+    let rec rev arr out =
+        match arr with
+        |h::t -> rev t (h::out)
+        |[] -> out
+
+    let toList = input |> List.ofSeq
+    rev toList [] |> System.String.Concat
