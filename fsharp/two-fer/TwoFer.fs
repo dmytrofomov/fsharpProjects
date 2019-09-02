da315890fa09abc5c3e9ed47e604f46f37da9a5b
fomov <fomov.innovitive@gmail.com>
2019-09-02 11:33:55 +0300

    Rfactor two-fer

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,6 +1,5 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string = 
-    match input with
-    | Some(input) -> "One for " + input + ", one for me."
-    | None -> "One for you, one for me."
+    Option.defaultValue "you" input  |> sprintf "One for %s, one for me." 
+    
