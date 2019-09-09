ccbb3fb28407df4b50796d8696cd2a57db2c35f5
fomov <fomov.innovitive@gmail.com>
2019-09-09 16:28:30 +0300

    Comm

---
+++ fsharp/diffie-hellman/DiffieHellman.fs
@@ -0,0 +1,10 @@
+﻿module DiffieHellman
+
+open System.Numerics
+
+let privateKey (primeP : BigInteger) =
+    System.Random().Next(2, int32 primeP - 1) |> BigInteger
+let publicKey primeP primeG privateKey =
+    BigInteger.ModPow(primeG, privateKey, primeP)
+let secret primeP publicKey privateKey =
+    BigInteger.ModPow(publicKey, privateKey, primeP)
