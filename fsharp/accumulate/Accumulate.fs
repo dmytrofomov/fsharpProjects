05fdd1bc7425651112f06b8110a37fb7c1a5cd5b
fomov <fomov.innovitive@gmail.com>
2019-11-19 10:53:37 +0200

    accumulatte

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,12 @@
+﻿module Accumulate
+
+let accumulate func input = 
+    let rec recursiveCall input func curRes =
+        match input with
+        | [] -> curRes
+        | h::t ->  recursiveCall t func (func h :: curRes)
+    recursiveCall input func []
+    |> List.rev
+     
+
+
